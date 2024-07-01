008de86b6f14a13a20867f18a4d48cd5cd722922
AceWalker001 <mwangangianthony1@gmail.com>
2024-07-01 16:45:14 +0300

    Implemented delete function and PubSub functionality

--- lib/real_time_todo_app_web/controllers/todo_live_controller.ex
+++ lib/real_time_todo_app_web/controllers/todo_live_controller.ex
@@ -4,11 +4,12 @@ defmodule RealTimeTodoAppWeb.TodoLiveController do
   alias RealTimeTodoApp.Todo
   alias RealTimeTodoApp.Repo
 
+
   def render(assigns) do
     ~H"""
       <div>Create your Todo here!</div>
       <%= for todo <- @todos do%>
-        <div><%= todo.name %></div>
+        <div><%= todo.name %></div><button phx-click = "delete" phx-value-id = {todo.id}>Delete Todo</button>
       <% end %>
       <form phx-submit = "create">
         <input type = "text" name = "name" id = "name">
@@ -18,21 +19,40 @@ defmodule RealTimeTodoAppWeb.TodoLiveController do
   end
 
   def mount(_params, _session, socket) do
+    if connected?(socket), do: Phoenix.PubSub.subscribe(RealTimeTodoApp.PubSub, "todos")
     {:ok, assign(socket, todos: Repo.all(Todo))}
   end
 
   def handle_event("create", %{"name" => name}, socket) do
-    {:noreply, assign(socket, todos: Repo.all(Todo))}
-    case Repo.insert(%Todo{name: name}) do
+    todo = %Todo{name: name}
+    case Repo.insert(todo) do
       {:ok, _message}->
         {:noreply, assign(socket, todos: Repo.all(Todo))}
         IO.inspect(name)
 
       {:error, changeset} ->
         {:noreply, changeset: changeset}
-
     end
+    Phoenix.PubSub.broadcast(RealTimeTodoApp.PubSub, "todos", {[:todo, :created], todo})
+    {:noreply, assign(socket, todos: Repo.all(Todo))}
 
   end
 
+  def handle_event("delete", %{"id" => id}, socket) do
+    todo = Repo.get!(Todo, id)
+    Repo.delete(todo)
+
+    Phoenix.PubSub.broadcast(RealTimeTodoApp.PubSub, "todos", {[:todo, :deleted], todo.id})
+
+    {:noreply, assign(socket, todos: Repo.all(Todo))}
+
+  end
+
+  def handle_info({[:todo, :created], _todo}, socket) do
+    {:noreply, assign(socket, todos: Repo.all(Todo))}
+  end
+
+  def handle_info({[:todo, :deleted], _todo_id}, socket) do
+    {:noreply, assign(socket, todos: Repo.all(Todo))}
+  end
 end
